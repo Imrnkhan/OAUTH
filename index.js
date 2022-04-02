9d42ac3f3434a275ef9ec4a513d1f4787f85fee9
Imrnkhan <imrankhan7581@gmail.com>
2022-04-02 22:42:31 +0530

    .

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {register, login, newToken}= require("./src/controllers/authController")
 app.use(express.json());
 
 const connect=()=>{
-    mongoose.connect(" mongodb://127.0.0.1:27017/Outh")
+    mongoose.connect(" mongodb://127.0.0.1:27017/Oauth")
 }
 const passport= require("./src/config/oauthconfig")
 
